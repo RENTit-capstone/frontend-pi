679e513d5f57bdd393fb9f0fd984a28f9caf178c
gurbur <jihwan7577@gmail.com>
2025-04-16 17:57:05 +0900

    Feat: test code(button & counter)

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 import { createState, render, apiFetch } from './core/core.js';
 
+const Counter = () => {
+    const [count, setCount] = createState(0);
+
+    return `
+        <div>
+            <h2> 카운트: ${count()}</h2>
+            <button onclick="(${() => setCount(count() + 1)})()">+1</button>
+        </div>
+    `;
+}
+
+render(Counter, document.getElementById('app'));
