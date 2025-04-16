86e41312962e8ba3ed517928016d34d3be0ac393
gurbur <jihwan7577@gmail.com>
2025-04-16 15:44:31 +0900

    Feat: simple apiFetch function

--- src/core/core.js
+++ src/core/core.js
@@ -36,6 +36,31 @@ export function render(componentFn, rootElement) {
 }
 
 /* ========== Fetch Helper ========== */
-export async function apiFetch() {
+export async function apiFetch(endpoint, { method = 'GET', body = null, headers = {} } = {}) {
+    const baseUrl = 'http://localhost:8000';
 
+    const options = {
+        method,
+        headers: {
+            'Content-Type': 'aplication/json',
+            ...headers,
+        },
+    };
+
+    if (body) {
+        options.body = JSON.stringify(body);
+    }
+
+    return fetch(`${baseUrl}${endpoint}`, options)
+        .then(async(res) => {
+            if (!res.ok) {
+                const error = await res.test();
+                throw new Error(`API Error (${res.status}): ${error}`);
+            }
+            return res.json();
+        })
+        .catch((err) => {
+            console.error('Fetch failed:', err,message);
+            throw err;
+        });
 }
