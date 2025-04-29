78097d2976f92b09d10e476ec4c26146063f97a0
gurbur <jihwan7577@gmail.com>
2025-04-30 01:23:51 +0900

    Refactor: refactoring Button css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 export default function Button({ id, label, onClick }) {
     return {
       html: `
-        <button id="${id}" style="padding: 16px 32px; font-size: 1.2rem; margin: 10px; width: 60%;">
+        <button id="${id}" class="common-button">
           ${label}
         </button>
       `,
