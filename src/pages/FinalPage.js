4087fb7c767dcdfd4341bdcdaabaa5a22d4e1780
gurbur <jihwan7577@gmail.com>
2025-05-05 18:31:53 +0900

    Feat: Final page

---
+++ src/pages/FinalPage.js
@@ -0,0 +1,12 @@
+export const FinalPage = ({ userName }) => {
+  return {
+    html: `
+      <div class="screen-container>
+        <h2>작업이 완료되었습니다.</h2>
+        <p>${userName}님, 이용해주셔서 감사합니다!</p>
+        <p class="loading-indicator">잠시 후 초기 화면으로 돌아갑니다...</p>
+      </div>
+    `,
+    handlers: {},
+  };
+};
